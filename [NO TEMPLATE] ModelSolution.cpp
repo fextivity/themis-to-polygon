15befc14d49a1082a63b02e16d69c8d18aeca94d
Sola Stunfell <trxbach135@gmail.com>
2020-12-06 16:06:05 +0700

    Create [NO TEMPLATE] ModelSolution.cpp

---
+++ [NO TEMPLATE] ModelSolution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+signed main(){
+    // Print content that is consistent with elemOufModel in Checker.cpp
+    cout << 69420;
+}
+
+/*
+==================================================+
+INPUT:                                            |
+--------------------------------------------------|
+
+--------------------------------------------------|
+==================================================+
+OUTPUT:                                           |
+--------------------------------------------------|
+
+--------------------------------------------------|
+==================================================+
+*/
